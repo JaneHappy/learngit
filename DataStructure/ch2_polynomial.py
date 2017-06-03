9f97cd18ec820349a59b635f3bc2865b36717b86
JaneHappy <1584360631@qq.com>
2017-06-03 22:28:01 +0800

    not finished

---
+++ DataStructure/ch2_polynomial.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Data Structure: chapter 2.4
+# unary polynomial, polynomial of one variable
+
+
+class polynomial(object):
+	"""docstring for polynomial"""
+	#def __init__(self, arg):
+	#	super(polynomial, self).__init__()
+	#	self.arg = arg
+	def __init__(self, item):
+		self.item = item
+
+	def add(self, num):
+		pass
